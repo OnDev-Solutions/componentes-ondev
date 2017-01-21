001e72295589a597163d9b5739c47ea36c783115
WalterJunior <walter.junior@projetusti.com.br>
2017-01-21 09:25:22 -0200

    Criado Componente Tabs

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp = require('gulp');
+var csso = require('gulp-csso');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+
+var paths = {
+    css: ['node_modules/bulma/css/bulma.css'],
+    scripts: ['node_modules/vue/dist/vue.js']
+};
+
+gulp.task('default', function() {
+    // Minify and copy all JavaScript (except vendor scripts)
+    pump([
+        gulp.src(paths.scripts),
+        uglify(),
+        gulp.dest('dist/js'),
+        gulp.src(paths.css),
+        csso(),
+        gulp.dest('dist/css')
+    ]);
+});
+
+gulp.task('development', function () {
+    pump([
+        gulp.src(paths.scripts),
+        uglify({
+            beautify: true,
+            bracketize: true,
+            comments: true
+        }),
+        gulp.dest('dist/js'),
+        gulp.src(paths.css),
+        csso({
+            restructure: true,
+            sourceMap: true,
+            debug: true
+        }),
+        gulp.dest('dist/css')
+    ]);
+});
