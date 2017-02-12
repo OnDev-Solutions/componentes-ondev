0273bfc6fe356cac65b0c09884fe9b8afb81d6bb
Walter Ribeiro <walterjunioranalise@gmail.com>
2017-02-12 16:14:18 -0200

    Novo Componente Panel

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,12 @@ var uglify = require('gulp-uglify');
 var pump = require('pump');
 
 var paths = {
-    css: ['node_modules/bulma/css/bulma.css'],
-    scripts: ['node_modules/vue/dist/vue.js']
+    css: ['node_modules/bulma/css/bulma.css', 'node_modules/font-awesome/css/font-awesome.min.css'],
+    scripts: ['node_modules/vue/dist/vue.js'],
+    fonts: ['node_modules/font-awesome/fonts/*.*']
 };
 
-gulp.task('default', function() {
+gulp.task('default', function () {
     // Minify and copy all JavaScript (except vendor scripts)
     pump([
         gulp.src(paths.scripts),
@@ -16,7 +17,11 @@ gulp.task('default', function() {
         gulp.dest('dist/js'),
         gulp.src(paths.css),
         csso(),
-        gulp.dest('dist/css')
+        gulp.dest('dist/css'),
+    ]);
+    pump([
+        gulp.src(paths.fonts),
+        gulp.dest('dist/fonts'),
     ]);
 });
 
